36ce46354f1140a7ce0fc73f5cf3acb1e74c2cdd
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2021-11-04 17:13:57 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+
+    std::srand(time(nullptr));
+
+    int a = std::rand() % 100;
+    int t = 10;
+
+    std::cout << "jeu du + || - a vous de trouver le bon nombre (10 essais)\n";
+
+    for (int i = 0; i < t; ++i)
+    {
+
+        std::cout << "Entre votre proposition :";
+
+        int b = 0;
+        std::cin >> b;
+        std::cin.ignore();
+
+        if (b == a)
+        {
+            std::cout << "Bravo !" << std::endl;
+            std::cin.ignore();
+        }
+
+        else if (b < a)
+        {
+            std::cout << "Plus" << std::endl;
+        }
+
+        else if (b > a)
+        {
+            std::cout << "Moin" << std::endl;
+        }
+
+        if (i == t - 1)
+        {
+            std::cout << "Perdu ! Vous avez grille vos essais" << std::endl;
+            std::cin.ignore();
+        }
+    }
+    return 0;
+}
